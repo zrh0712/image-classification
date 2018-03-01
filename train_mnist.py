e0d8f0ce1392b39bdf4863d59e9d9169ec3d0ff5
zhuriheng <35758033+zhuriheng@users.noreply.github.com>
2018-03-01 18:47:13 +0800

    bug fixed: use np.frombuffer instead of fromstring

--- train_mnist.py
+++ train_mnist.py
@@ -35,10 +35,12 @@ def read_data(label, image):
     base_url = 'http://yann.lecun.com/exdb/mnist/'
     with gzip.open(download_file(base_url+label, os.path.join('data',label))) as flbl:
         magic, num = struct.unpack(">II", flbl.read(8))
-        label = np.fromstring(flbl.read(), dtype=np.int8)
+        label = np.frombuffer(flbl.read(), dtype=np.int8)
+        #label = np.fromstring(flbl.read(), dtype=np.int8)
     with gzip.open(download_file(base_url+image, os.path.join('data',image)), 'rb') as fimg:
         magic, num, rows, cols = struct.unpack(">IIII", fimg.read(16))
-        image = np.fromstring(fimg.read(), dtype=np.uint8).reshape(len(label), rows, cols)
+        image = np.frombuffer(fimg.read(), dtype=np.uint8).reshape(len(label), rows, cols)
+        #image = np.fromstring(fimg.read(), dtype=np.uint8).reshape(len(label), rows, cols)
     return (label, image)
 
 
