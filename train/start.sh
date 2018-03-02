be4bf5a37c38ecf48b3dfd186b83035010866d9c
zhuriheng <35758033+zhuriheng@users.noreply.github.com>
2018-03-02 18:12:54 +0800

    Update start.sh
    prepare model and dataset

--- train/start.sh
+++ train/start.sh
@@ -1,2 +1,14 @@
+echo "downloading example mxnet resnet50 model"
+mkdir -p model/
+cd model/
+wget http://data.mxnet.io/models/imagenet/resnet/50-layers/resnet-50-0000.params
+wget http://data.mxnet.io/models/imagenet/resnet/50-layers/resnet-50-symbol.json
+cd ..
+
+echo "downloading example dataset cifar10"
+mkdir -p data/
+wget http://data.mxnet.io/data/cifar10/cifar10_train.rec
+wget http://data.mxnet.io/data/cifar10/cifar10_val.rec
+
 echo "start to run training script"
-python mxnet_train.py 
+python mxnet_train.py log.log -f -s 
